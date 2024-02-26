280fa1fb5b2aecbf98864814ddcaad6d5fd98f33
Rahul Kottak <rahulkottak@gmail.com>
2024-02-26 17:31:53 +0530

    Go

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e42a59e6-bf58-4250-97e5-5610f581aae3';
+export const MODAL_CLOSE_SEC = 2.5;
